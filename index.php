85e8de18355f46fad5ce4ee5d042aa4ccacfeb82
Alexey Kostarev <kaf@altlinux.org>
2021-09-01 11:16:57 +0500

    Add root index.php with basics links

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>Links</title>
+</head>
+<body>
+
+<h1>TEST</h1>
+
+<h2>Sisyphus</h2>
+<ul>
+<li><a href='http://getacos.altlinux.org/v1/graph/?stream=sisyphus&basearch=x86_64'>Graph</a></li>
+<li><a href='http://getacos.altlinux.org/ostree/update/?ref=acos/x86_64/sisyphus&version=sisyphus.20210830.0.0'>Update</a></li>
+</ul>
+
+<h2>p10</h2>
+<ul>
+<li>
+</ul>
+
+<h1>ACOS Clients</h1>
+
+<h2>Sisyphus</h2>
+<ul>
+<li>
+</ul>
+
+<h2>p10</h2>
+<ul>
+<li>
+</ul>
+
