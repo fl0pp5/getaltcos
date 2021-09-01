f09d056dbc232a69ba96e3894459c9c6d8a72406
Alexey Kostarev <kaf@altlinux.org>
2021-09-01 13:07:24 +0500

    Tune index.php

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
+<?php
+//phpinfo();
+function listStreams() {
+  $fd = opendir($_SERVER['DOCUMENT_ROOT'] . "/ACOS/streams/acos/x86_64");
+  $ret = [];
+  while ($entry=readdir($fd)) {
+    if (substr($entry,0,1) == '.') continue;
+    $ret[] = $entry;
+  } 
+  return $ret;
+}
+
+
+function getRefs($stream, $typeRepo='bare') {
+  $repoDir = $_SERVER['DOCUMENT_ROOT'] . "/ACOS/streams/acos/x86_64/$stream/$typeRepo/repo";
+  $cmd = "ostree refs --repo=$repoDir";
+  $output = [];
+  //echo "<pre>CMD=$cmd</pre>\n";
+  exec($cmd, $output);
+  //echo "<pre>REFS=" . print_r($output, 1) . "</pre>\n";
+  return $output; 
+}
+
+function getLog($ref, $typeRepo='bare') {
+  $repoDir = $_SERVER['DOCUMENT_ROOT'] . "/ACOS/streams/$ref/$typeRepo/repo";
+  $cmd = "ostree log $ref --repo=$repoDir";
+  $output = [];
+  echo "<pre>CMD=$cmd</pre>\n";
+  exec($cmd, $output);
+  echo "<pre>REFS=" . print_r($output, 1) . "</pre>\n";
+  return $output;  
+}
+
+?>
+
 <html>
 <head>
 <title>Links</title>
@@ -6,11 +41,47 @@
 
 <h1>TEST</h1>
 
+<?php
+$streams = listStreams();
+echo "<pre>STREAMS=" . print_r($streams, 1) . "</pre>\n"; 
+foreach ($streams as $stream) {
+?>
+<ul><h2>Поток: <?= $stream?></h2>
+<?php
+  $refs = getRefs($stream);
+  foreach ($refs  as $ref) {
+?>
+  <li>
+    <ul><h3>REF: <?= $ref?></h3>
+    <li><a href='http://<?= $_SERVER['HTTP_HOST']?>/v1/graph/?stream=<?= $stream?>&basearch=x86_64' target='ostreeREST'>ГРАФ</a></li>
+<?php
+    $logs = getLog($ref);
+?>
+    </ul>
+  </li>
+<?php
+  }
+?>
+</ul>
+<?php
+}
+?>
+
+
+
 <h2>Sisyphus</h2>
+
+<h3>Граф</h3>
+
 <ul>
 <li><a href='http://getacos.altlinux.org/v1/graph/?stream=sisyphus&basearch=x86_64'>Graph</a></li>
 <li><a href='http://getacos.altlinux.org/ostree/update/?ref=acos/x86_64/sisyphus&version=sisyphus.20210830.0.0'>Update</a></li>
 </ul>
+
+<h3>OSTREE</h3>
+
+
+
 
 <h2>p10</h2>
 <ul>
--- v1/graph/index.php
+++ v1/graph/index.php
@@ -1,5 +1,46 @@
 <?php
 //phpinfo();
+function getCommits($repo, $ref) {
+  $cmd = "ostree --repo=$repo log $ref";
+  //echo "CMD=$cmd<br>\n";
+  exec($cmd, $output);
+
+  #print_r($output);
+  $commits = [];
+  $commit = [];
+  for ($i = 0; $i < count($output); $i++) {
+      $str = trim($output[$i]);
+      #echo $str;
+      if (strlen($str) ==0 ) {
+        $commits[$commitId] = $commit;
+        $commit = [];
+        continue;
+      }
+      if (substr($str, 0, 6) == 'commit') {
+        $commitId = trim(substr($str,6));
+        $commit['commit'] = $commitId;
+        continue; 
+      }
+      if (substr($str, 0, 7) == 'Parent:') {
+        $parent = trim(substr($str,7));
+        $commit['Parent'] = $parent;
+        continue;
+      }
+      if (substr($str, 0, 5) == 'Date:') {
+        $date = trim(substr($str,5));
+        $commit['Date'] = $date;
+        continue;
+      }
+      if (substr($str, 0, 8) == 'Version:') {
+        $version = trim(substr($str, 8));
+        $commit['Version'] = $version;
+        continue;
+      }
+    }
+  uasort($commits, 'cmpByDate');
+  return $commits;
+}
+
 $basearch = @$_REQUEST['basearch'];
 if (strlen($basearch) ==0 ) {
   errorReply(1, 'Parameter basearch is not defined');
@@ -19,45 +60,7 @@ if (!file_exists("$repo/config")) {
 }
 
 $ref = "acos/$basearch/$stream";
-
-$cmd = "ostree --repo=$repo log $ref";
-#echo "CMD=$cmd<br>\n";
-exec($cmd, $output);
-
-#print_r($output);
-$commits = [];
-$commit = [];
-for ($i = 0; $i < count($output); $i++) {
-    $str = trim($output[$i]);
-    #echo $str;
-    if (strlen($str) ==0 ) {
-      $commits[$commitId] = $commit;
-      $commit = [];
-      continue;
-    }
-    if (substr($str, 0, 6) == 'commit') {
-      $commitId = trim(substr($str,6));
-      $commit['commit'] = $commitId;
-      continue; 
-    }
-    if (substr($str, 0, 7) == 'Parent:') {
-      $parent = trim(substr($str,7));
-      $commit['Parent'] = $parent;
-      continue;
-    }
-    if (substr($str, 0, 5) == 'Date:') {
-      $date = trim(substr($str,5));
-      $commit['Date'] = $date;
-      continue;
-    }
-    if (substr($str, 0, 8) == 'Version:') {
-      $version = trim(substr($str, 8));
-      $commit['Version'] = $version;
-      continue;
-    }
-}
-uasort($commits, 'cmpByDate'); 
-
+$commits = getCommits($repo, $ref);
 $index = 0;
 foreach($commits as $id => $commit) {
   $commits[$id]['index'] = "" . $index;
