6a3bec5c562f61c489eb11700dfcff79eaf78b6d
Alexey Kostarev <kaf@altlinux.org>
2021-09-07 11:36:46 +0500

    Debug

--- ostree/bin/mkimage-profiles.sh
+++ ostree/bin/mkimage-profiles.sh
@@ -16,8 +16,10 @@ BRANCH=$1
 if [ $BRANCH = 'sisyphus' ]
 then
 	REPOBRANCH='Sisyphus'
+	NS=alt
 else
 	REPOBRANCH=$BRANCH/branch
+	NS=$BRANCH
 fi
 
 ref=acos/x86_64/$BRANCH
@@ -57,9 +59,7 @@ if [ ! -f ~/apt/cache/$BRANCH/archives/partial ]
 then
 	mkdir -p ~/apt/cache/$BRANCH/archives/partial
 fi
-if [ ! -f ~/apt/apt.conf.$BRANCH.x86_64 ]
-then
-	cat <<EOF > ~/apt/apt.conf.$BRANCH.x86_64
+cat <<EOF > ~/apt/apt.conf.$BRANCH.x86_64
 Dir::Etc::SourceList "$HOME/apt/sources.list.$BRANCH.x86_64";
 Dir::Etc::SourceParts /var/empty;
 Dir::Etc::main "/dev/null";
@@ -68,13 +68,11 @@ APT::Architecture "64";
 Dir::State::lists "$HOME/apt/lists/";
 Dir::Cache "$HOME/apt/cache/$BRANCH/";
 EOF
-fi
-if [ ! -f ~/apt/sources.list.$BRANCH.x86_64 ]
-then
-	cat <<EOF > ~/apt/sources.list.$BRANCH.x86_64
-rpm [alt] http://ftp.altlinux.org/pub/distributions/ALTLinux/ $REPOBRANCH/x86_64 classic
-rpm [alt] http://ftp.altlinux.org/pub/distributions/ALTLinux/ $REPOBRANCH/noarch classic
+
+cat <<EOF > ~/apt/sources.list.$BRANCH.x86_64
+rpm [$NS] http://ftp.altlinux.org/pub/distributions/ALTLinux/ $REPOBRANCH/x86_64 classic
+rpm [$NS] http://ftp.altlinux.org/pub/distributions/ALTLinux/ $REPOBRANCH/noarch classic
 EOF
-fi
+
 cd $MKIMAGEDIR
 make DEBUG=1 APTCONF=~/apt/apt.conf.$BRANCH.x86_64 BRANCH=$BRANCH ARCH=x86_64 vm/acos.tar
