5e9f271b3ea31f32e97b7303ddd1252ae21c033c
Alexey Kostarev <kaf@altlinux.org>
2021-11-09 11:25:36 +0500

    Возврат в копировании репо к копирования по списку коммитов

--- ostree/pullToArchive/index.php
+++ ostree/pullToArchive/index.php
@@ -17,22 +17,22 @@ if (!$archiveRepo->haveConfig()) {
   $archiveRepo->init();
 }
 
-$cmd = "sudo ostree pull-local --depth=-1 " . $repo->repoDir . " $ref --repo=". $archiveRepo->repoDir .' 2>&1';;
-$output = [];
-echo "<pre>CMD OSTREE PULL MIRROR=$cmd</pre>\n";
-exec($cmd, $output);
-echo "<pre>OUTPUT OSTREE PULL MIRRIR=" . print_r($output, 1) . "</pre>\n";
+// $cmd = "sudo ostree pull-local --depth=-1 " . $repo->repoDir . " $ref --repo=". $archiveRepo->repoDir .' 2>&1';;
+// $output = [];
+// echo "<pre>CMD OSTREE PULL MIRROR=$cmd</pre>\n";
+// exec($cmd, $output);
+// echo "<pre>OUTPUT OSTREE PULL MIRROR=" . print_r($output, 1) . "</pre>\n";
 
 
-// $commits = $repo->getCommits($ref);
-//
-// $commitIds = array_keys($commits);
-//
-// foreach ($commitIds as $commitId) {
-//     $cmd = "sudo ostree pull-local " . $repo->repoDir . " $ref $commitId --repo=".$archiveRepo->repoDir .' 2>&1';;
-//     $output = [];
-//     echo "<pre>CMD=$cmd</pre>\n";
-//     exec($cmd, $output);
-//     echo "<pre>REFS=" . print_r($output, 1) . "</pre>\n";
-// }
+$commits = $repo->getCommits($ref);
+
+$commitIds = array_keys($commits);
+
+foreach ($commitIds as $commitId) {
+    $cmd = "sudo ostree pull-local " . $repo->repoDir . " $ref $commitId --repo=".$archiveRepo->repoDir .' 2>&1';;
+    $output = [];
+    echo "<pre>CMD=$cmd</pre>\n";
+    exec($cmd, $output);
+    echo "<pre>REFS=" . print_r($output, 1) . "</pre>\n";
+}
 
