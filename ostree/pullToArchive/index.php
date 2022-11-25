dcb1d1105011620f922bb44194b234b799741e84
Alexey Kostarev <kaf@altlinux.org>
2022-11-25 11:29:58 +0500

    Исправлена странная ошибка формирования идентификатора последнего комита с датой старше 2022 года

--- ostree/pullToArchive/index.php
+++ ostree/pullToArchive/index.php
@@ -17,22 +17,33 @@ if (!$archiveRepo->haveConfig()) {
   $archiveRepo->init();
 }
 
-// $cmd = "sudo ostree pull-local --depth=-1 " . $repo->repoDir . " $ref --repo=". $archiveRepo->repoDir .' 2>&1';;
-// $output = [];
-// echo "<pre>CMD OSTREE PULL MIRROR=$cmd</pre>\n";
-// exec($cmd, $output);
-// echo "<pre>OUTPUT OSTREE PULL MIRROR=" . print_r($output, 1) . "</pre>\n";
+
 
 
 $commits = $repo->getCommits($ref);
+$lastCommitId = $repo->lastCommitId;
 
-$commitIds = array_keys($commits);
+$cmd = "sudo ostree pull-local --depth=-1 " . $repo->repoDir . " $ref $lastCommitId --repo=". $archiveRepo->repoDir .' 2>&1';;
+$output = [];
+echo "<pre>CMD OSTREE PULL MIRROR=$cmd</pre>\n";
+exec($cmd, $output);
+echo "<pre>OUTPUT OSTREE PULL MIRROR=" . print_r($output, 1) . "</pre>\n";
 
-foreach ($commitIds as $commitId) {
-    $cmd = "sudo ostree pull-local " . $repo->repoDir . " $ref $commitId --repo=".$archiveRepo->repoDir .' 2>&1';;
-    $output = [];
-    echo "<pre>CMD=$cmd</pre>\n";
-    exec($cmd, $output);
-    echo "<pre>REFS=" . print_r($output, 1) . "</pre>\n";
-}
+// $commitIds = array_keys($commits);
+//
+// foreach ($commitIds as $commitId) {
+//     $cmd = "sudo ostree pull-local " . $repo->repoDir . " $ref $commitId --repo=".$archiveRepo->repoDir .' 2>&1';;
+//     $output = [];
+//     echo "<pre>CMD=$cmd</pre>\n";
+//     exec($cmd, $output);
+//     echo "<pre>REFS=" . print_r($output, 1) . "</pre>\n";
+// }
 
+$refFile = $archiveRepo->repoDir . "/refs/heads/" . $archiveRepo->ref;
+
+$cmd = "sudo echo $lastCommitId | sudo  tee $refFile";
+// $cmd = "sudo cat >$refFile <<EOF\n$lastCommitId\nEOF\n";
+echo "<pre>CMD=$cmd</pre>\n";
+$output = [];
+exec($cmd, $output);
+echo "<pre>RES=" . print_r($output, 1) . "</pre>\n";
