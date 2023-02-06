22b00bec8a8fd2913a1816089a5fe74b5ece8c28
fl0pp5 <fl0pp5@mail.ru>
2023-02-06 19:14:46 +0300

    Update "Action" class

--- actions/__init__.py
+++ actions/__init__.py
@@ -5,9 +5,10 @@ import subprocess
 class Action:
     _TMPL_CMD = string.Template("$cmd")
 
-    def start(self, **kwargs):
+    @classmethod
+    def do(cls, **kwargs):
         return subprocess.run(
-            self._TMPL_CMD.substitute(kwargs),
+            cls._TMPL_CMD.substitute(kwargs),
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
